b86f38daeafea187e8102ac2d9c100f3f1ac2f75
Saksham Yadav <sushil68083@gmail.com>
2026-08-18 00:04:43 +0530

    Create MatrixMultiplication.java

---
+++ Matrices/MatrixMultiplication.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class MatrixMultiplication{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter rows of first matrix: ");
+        int rows1 = sc.nextInt();
+
+        System.out.print("Enter columns of first matrix: ");
+        int columns1 = sc.nextInt();
+
+        System.out.print("Enter rows of second matrix: ");
+        int rows2 = sc.nextInt();
+
+        System.out.print("Enter columns of second matrix: ");
+        int columns2 = sc.nextInt();
+
+        if (columns1 != rows2){
+            System.out.println("Matrix multiplication is not possible.");
+            sc.close();
+            return;
+        }
+
+        int[][] matrix1 = new int[rows1][columns1];
+        int[][] matrix2 = new int[rows2][columns2];
+        int[][] result = new int[rows1][columns2];
+
+        System.out.println("Enter elements of first matrix:");
+
+        for (int i = 0; i < rows1; i++){
+            for (int j = 0; j < columns1; j++){
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter elements of second matrix:");
+
+        for (int i = 0; i < rows2; i++){
+            for (int j = 0; j < columns2; j++){
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < rows1; i++){
+            for (int j = 0; j < columns2; j++){
+                for (int k = 0; k < columns1; k++){
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+
+        System.out.println("Resultant Matrix:");
+
+        for (int i = 0; i < rows1; i++){
+            for (int j = 0; j < columns2; j++){
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
