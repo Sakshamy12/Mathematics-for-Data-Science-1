5292b25aaf2e48ac22f63c69bd8a739886251e47
Saksham Yadav <sushil68083@gmail.com>
2026-08-17 23:42:02 +0530

    Update and rename Problem07.java to Question1.java

--- Paper1/Question1.java
+++ Paper1/Question1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class Problem07 {
-    public static void main(String[] args) {
+public class Question1{
+    public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
 
@@ -12,16 +12,16 @@ public class Problem07 {
 
         System.out.println("Enter 0 for no road and 1 for road:");
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
                 matrix[i][j] = sc.nextInt();
             }
         }
 
         System.out.println("Road Matrix:");
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
@@ -29,9 +29,9 @@ public class Problem07 {
 
         System.out.println("Roads:");
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (matrix[i][j] == 1) {
+        for (int i = 0; i < n; i++){
+            for (int j = i + 1; j < n; j++){
+                if (matrix[i][j] == 1){
                     System.out.println("City " + i + " to City " + j);
                 }
             }
